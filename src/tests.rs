df3f4166102ee9995cf55d2461a28f756f2e46cd
mode80 <mode80@users.noreply.github.com>
2022-03-06 20:08:00 -0700

    separate tests into tests.rs

---
+++ src/tests.rs
@@ -0,0 +1,36 @@
+use super::*;
+// use test::Bencher;
+
+// #[test]
+fn score_slot_test() {
+    assert_eq!(15, score_slot(FIVES,[1,2,5,5,5]));
+}
+
+#[test]
+fn best_dice_ev_test() {
+    let slots= array_vec!([u8;13] => 7,13);
+    // let slots= array_vec!([u8;13] => 1,2,3,4,5,6,7,8,9,10,11,12,13);
+    let game_state = &GameState{
+        sorted_open_slots: slots,
+        sorted_dievals: [1,2,5,5,5],
+        rolls_remaining: 1,
+        upper_bonus_deficit: INIT_DEFICIT,
+        yahtzee_is_wild: false,
+    };
+    let slot_count=game_state.sorted_open_slots.len();
+    let combo_count = (1..=slot_count).map(|r| n_take_r(slot_count as u128, r as u128,false,false) as u64 ).sum() ;
+    let app_state = & mut AppState{
+        progress_bar : Arc::new(RwLock::new(ProgressBar::new(combo_count))), 
+        done : Arc::new(DashSet::new()) ,  
+        ev_cache : Arc::new(DashMap::new()),
+    };
+    ev_for_state(game_state,app_state);
+}
+
+
+// // #[bench]
+// fn score_slot_bench(b: &mut Bencher) {
+//     b.iter(best_dice_ev_test);
+// }
+
+
